cc20e8fb6eea822b636675bac366b41a36e5659c
csauve <connorsauve@gmail.com>
2013-11-04 22:12:00 -0800

    honour the dead

---
+++ modules/rip.js
@@ -0,0 +1,10 @@
+module.exports = function(client, config) {
+	config.rip = ".*(deebot|kanye|rec0).*";
+
+    client.addListener("message", function(from, to, message) {
+        var match = message.match(new RegExp(config.rip, "i"));
+        if (match) {
+            client.say(to, "rip " + match[1]);
+        }
+    });
+};
