a89249cc21b638e20647c456caecd6743b0f6400
csauve <connorsauve@gmail.com>
2013-11-04 22:24:31 -0800

    Halo 3 is illegal

---
+++ modules/halo3.js
@@ -0,0 +1,9 @@
+module.exports = function(client, config) {
+	regex = new RegExp(".*(halo|h4lo|H410|hal0|ha1o|h@lo|halO|\|-\|/-\|_\(\)|hola|h0la)(.*)(tres|tripple|triple|III|three|3|1\+2|2\+1|4-1).*", "i");
+
+    client.addListener("message", function(from, to, message) {
+        if (message.match(regex)) {
+            client.say(to, ".k " + from + " halo 3");
+        }
+    });
+};
