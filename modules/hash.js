47f6f5f73bce13ce1b993312e2e0f55d549304d6
csauve <connorsauve@gmail.com>
2013-11-06 13:33:43 -0800

    Updating hash module

--- modules/hash.js
+++ modules/hash.js
@@ -1,18 +1,22 @@
-var crypto = require('crypto');
-module.exports = function(client,config){
-	client.addListener("message#",function(nick,to,text){
-		var cmd = text.match(RegExp(config.cmdPrefix + "hash\\s+(.+)\\s(.+)","i"));
+var crypto = require("crypto");
+
+module.exports = function(client, config) {
+	client.addListener("message#", function(nick, to, text) {
+		var cmd = text.match(RegExp(config.cmdPrefix + "hash(?:\\s+(.+)\\s(.+))?", "i"));
 		if (cmd) {
-			//list of available hash functions is dependent on the system
-			//list of available functions available through:
-			//	"openssl list-message-digest-algorithms"
-			var hash = crypto.createHash(cmd[0]).update(cmd[1]).digest("hex");
-			client.say(to,cmd[0] + " hash of \"" + cmd[1] +"\": " + hash);
-		};
-		else
-		{
-			//needs some cooler way to print help when it's needed
-			client.say(to,"hash (HashType) (string)");
+			if (cmd[1] && cmd[2]) {
+				try {
+					var hash = crypto.createHash(cmd[1]).update(cmd[1]).digest("hex");
+					client.say(to, cmd[1] + " hash of \"" + cmd[2] +"\": " + hash);
+				} catch (err) {
+					client.say(to, nick + ": Error: " + err);
+				}
+			} else {
+				//list of available hash functions is dependent on the system
+				//list of available functions available through:
+				//	"openssl list-message-digest-algorithms"
+				client.say(to, nick + ": Supported hash functions: " + crypto.getHashes().join(", "));
+			}
 		}
 	});
 }
