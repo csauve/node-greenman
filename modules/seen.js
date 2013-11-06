14e7dc61c48ef39ea9e270b2379d07117b0c41ec
csauve <connorsauve@gmail.com>
2013-11-06 12:07:05 -0800

    Fixed seen.js

--- modules/seen.js
+++ modules/seen.js
@@ -1,27 +1,42 @@
-var nStore = require('nstore');
+var nStore = require("nstore");
+var moment = require("moment");
+var seen = nStore.new("seen.db", function() {});
+
+function saveSeen(nick, channel) {
+	seen.save(nick,
+		{
+			date: new Date(),
+			channel: channel
+		},
+		function(error) {
+			if (error) {
+				console.log(error);
+			}
+		}
+	);
+}
 
 module.exports = function(client, config) {
 	//save the timestamp upon joining
-	client.addListener("join", function(channel,nick,message) {
-		var seen = nStore.new("seen.db", function() {
-			seen.save(channel,
-				{
-					Name: nick,
-					Time: new Date()
-				}
-			);
-		});
+	client.addListener("join", function(channel, nick, message) {
+		saveSeen(nick, channel);
 	});
 
 	client.addListener("message#", function(nick, to, text) {
-		var cmd = text.match(RegExp("^(\\" + config.cmdPrefix + "seen)\s(.+)","i"));
-		if (cmd && cmd[1]) {
+		//save the timestamp upon message from nick
+		saveSeen(nick, to);
+
+		//check if theyre issuing the seen command
+		var cmd = text.match(RegExp(config.cmdPrefix + "seen\\s+(.+)", "i"));
+		if (cmd) {
 			var name = cmd[1];
 			seen.get(name, function(err, doc, key) {
 				if (err) {
-					console.log(err);
+					client.say(to, nick + ": I haven't seen " + name);
+					return;
 				}
-				client.say(name + " was last seen " + doc["Time"]);
+				client.say(to, nick + ": I last saw " + name + " in " + doc["channel"] +
+					" " + moment(doc["date"]).fromNow());
 			});
 		}
 	});
