28463303b075e6e5d4a604b08abb56249dcdb724
csauve <connorsauve@gmail.com>
2013-12-26 02:23:03 -0800

    Added some response messages to rcon module and added a dice module

---
+++ modules/dice.js
@@ -0,0 +1,29 @@
+var ircClient = require("../ircClient");
+var config = require("../config");
+
+function handleMessage(from, to, message) {
+    var match = message.match(RegExp(config.cmdPrefix + "d\\s+(\\d+)", "i"));
+    if (match) {
+        var sides = parseInt(match[1], 10);
+        if (sides == NaN) {
+            ircClient.say(to, from + ": That's not a number.");
+        }
+
+        var roll = Math.floor((Math.random() * sides) + 1);
+        if (from == "con" && sides == 5) {
+            roll = 5;
+        }
+
+        ircClient.say(to, from + ": A " + roll + " shows on the " + sides + "-sided die.")
+    }
+}
+
+module.exports = {
+    setup: function() {
+        ircClient.addListener("message#", handleMessage);
+    },
+
+    shutdown: function() {
+        ircClient.removeListener("message#", handleMessage);
+    }
+};
